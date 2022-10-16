f33d674d03e57dea7838c3f7e4c6836be2d1b78f
Summer Jade <summerjadefrosal@gmail.com>
2022-10-15 21:37:09 -0400

    added components

--- my-portfolio-react-app/src/App.js
+++ my-portfolio-react-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom"
+import Home from './components/Home'
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Portfolio from "./components/Portfolio"
+import Resume from "./components/Resume"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>HOME</Link>
+          </li>
+          <li>
+            <Link to='about'>ABOUT ME</Link>
+          </li>
+          <li>
+            <Link to='portfolio'>PORTFOLIO</Link>
+          </li>
+          <li>
+            <Link to='contact'>CONTACT ME</Link>
+          </li>
+          <li>
+            <Link to='resume'>RESUME</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="main">
+        {/* this defines all the routes */}
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='about' element={<About />}></Route>
+          <Route path='portfolio' element={<Portfolio />}></Route>
+          <Route path='contact' element={<Contact />}></Route>
+          <Route path='about' element={<Resume />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
+
 
 export default App;
