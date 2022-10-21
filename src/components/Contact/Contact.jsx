0a6e4a6ee13e820f85b0516f51ea63271641e503
Summer Jade <summerjadefrosal@gmail.com>
2022-10-21 15:45:42 -0400

    added contact form functionality

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
-
+  
 const Contact = () => {
-  return (
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errors, setErrors] = useState([]);
+  
+    const isValidEmail = email => {
+      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regex.test(String(email).toLowerCase());
+    };
+  
+  const handleSubmit = () => {
+    let errorsArr = [];
+    if (!name) errorsArr.push('Name is required!');
+    if (!email) errorsArr.push('Email is required!');
+    if(email && !isValidEmail(email)) errorsArr.push('Email is invalid!');
+    if (!message) errorsArr.push('Message is required!');
+
+    setErrors(errorsArr);
+  }
+
+    return (
     <div>
       {/* <!-- CONTACT --> */}
         <section className="contact section" id="contact">
@@ -10,11 +30,16 @@ const Contact = () => {
 
             <div className="contact_container bd-grid">
                 <form action="" className="contact_form">
-                    <input type="text" placeholder="Name" className="contact_input"/>
-                    <input type="mail" placeholder="Email" className="contact_input"/>
-                    <textarea name="" id="" cols="0" rows="10" className="contact_input"></textarea>
-                    <input type="button" value="Enter" className="contact_button button"/>
-                </form>
+                    <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} className="contact_input"/>
+                    <input type="mail" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="contact_input"/>
+                    <textarea name="" id="" cols="0" rows="10" value={message} onChange={e => setMessage(e.target.value)} className="contact_input"></textarea>
+                    <input type="button" value="Enter" onClick={handleSubmit} className="contact_button button"/>
+                    <div className={errors.length > 0 ? 'visible' : ''}>
+                      <ul>
+                        {errors.map((err, index) => (<li key={index}>{err}</li> ))}
+                      </ul>
+                    </div>
+              </form>
             </div>
         </section>
     </div>
